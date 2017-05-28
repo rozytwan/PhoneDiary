423025f879b6c3363806dab572cfdcf59932a2dc
rozytwan <rozytwan@gmail.com>
2017-05-28 09:39:14 +0545

    Database first approach an joins

---
+++ PhoneDirectory/Controllers/ContactController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PhoneDirectory.Models;
+using System.IO;
+using System.Net;
+
+
+namespace PhoneDirectory.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+
+        Entities1 dB = new Entities1();
+        Contact objContact = new Contact();
+        public ActionResult Index()
+        {
+            objContact.contactList = dB.Contacts.ToList();
+
+            return View("Index", objContact.contactList);
+        }
+
+        public ActionResult AddContact()
+        {
+            ViewBag.UserId = new SelectList(dB.AspNetUsers, "Id", "Email");
+            ViewBag.CountryId = new SelectList(dB.Countries, "CountryId", "CountryName");
+            ViewBag.StateId = new SelectList(dB.States, "StateId", "StateName");
+
+            return View();
+        }
+
+        Contact objCont = new Contact();
+
+        [HttpPost]
+        public ActionResult Save(Contact objContact, HttpPostedFileBase file)
+        {
+            ViewBag.IsSaved = false;
+
+            if (ModelState.IsValid)
+            {
+
+                var fileName = "";
+                if (file.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
+                    string[] file_name = fileName.Split('.');
+                    file.SaveAs(path);
+                }
+                objCont.ContactName = objContact.ContactName;
+                objCont.ContactNo1 = objContact.ContactNo1;
+                objCont.ContactNo2 = objContact.ContactNo2;
+                objCont.Image = "Content/Images/" + fileName;
+                objCont.Address = objContact.Address;
+                objCont.CountryId = objContact.CountryId;
+                objCont.StateId = objContact.StateId;
+                objCont.UserId = objContact.UserId;
+                dB.Contacts.Add(objCont);
+                dB.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            ViewBag.UserId = new SelectList(dB.AspNetUsers, "Id", "Email", objContact.UserId);
+            ViewBag.CountryId = new SelectList(dB.Countries, "CountryId", "CountryName", objContact.CountryId);
+            ViewBag.StateId = new SelectList(dB.States, "StateId", "StateName", objContact.StateId);
+            return View();
+
+        }
+        public ActionResult EditContact(int? id)
+        {
+            if (!string.IsNullOrEmpty(id.ToString()))
+            {
+
+                //objContact = dB.Contacts.Find(id);
+                //ViewBag.CountryId = new SelectList(dB.Countries, "CountryId", "CountryName", objContact.CountryId);
+                //ViewBag.StateId = new SelectList(dB.States, "StateId", "StateName", objContact.StateId);
+
+
+                return View(objContact);
+            }
+            else
+            {
+                return View("~/Views/Shared/ErrorAdmin.cshtml");
+            }
+        }
+        public ActionResult Edit(Contact contact, int id, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                objContact = dB.Contacts.Find(id);
+                var fileName = "";
+                if (file.ContentLength > 0)
+                {
+                    fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Content/Images"), fileName);
+                    string[] file_name = fileName.Split('.');
+                    file.SaveAs(path);
+                }
+                objContact.ContactName = contact.ContactName;
+                objContact.ContactNo1 = contact.ContactNo1;
+                objContact.ContactNo2 = contact.ContactNo2;
+                objContact.Image = "Content/Images/" + fileName;
+                objContact.Address = contact.Address;
+                objContact.CountryId = contact.CountryId;
+                objContact.StateId = contact.StateId;
+                objContact.UserId = contact.UserId;
+                dB.Entry(contact);
+                dB.Contacts.Add(objContact);
+                dB.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            ViewBag.UserId = new SelectList(dB.AspNetUsers, "Id", "Email", objContact.UserId);
+            ViewBag.CountryId = new SelectList(dB.Countries, "CountryId", "CountryName", objContact.CountryId);
+            ViewBag.StateId = new SelectList(dB.States, "StateId", "StateName", objContact.StateId);
+
+            return View(objContact);
+
+        }
+        Contact cont = new Contact();
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cont = dB.Contacts.Find(id);
+            if (cont == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cont);
+        }
+
+    [HttpPost, ActionName("Delete")]
+        public ActionResult Remove(int id)
+        {
+            objContact= dB.Contacts.Find(id);
+            dB.Contacts.Remove(objContact);
+            dB.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    
+    
+    public ActionResult SearchNew(string search="",string field="",bool ascending=true )
+        {
+            ViewBag.search = search;
+            ViewBag.ascending = ascending;
+
+            var result = dB.Contacts
+    .Where(t =>
+        t.ContactName.Contains(search) ||
+        t.ContactNo1.Contains(search) ||
+        t.ContactNo2.Contains(search) ||
+        t.Address.Contains(search) ||
+         t.State.StateName.Contains(search) ||
+        t.Country.CountryName.Contains(search));
+        
+            if (field == "name")
+            {
+                if (ascending)
+                    result = result.OrderBy(p => p.ContactName);
+                else
+                    result = result.OrderByDescending(p => p.ContactName);
+            }
+            if (field=="Country")
+            {
+                if (ascending)
+                {
+                    result = result.OrderBy(p => p.Country.CountryName);
+                }
+                else
+                    result = result.OrderByDescending(p => p.Country.CountryName);
+               
+            }
+            if (field=="State")
+            {
+                if (ascending)
+                {
+                    result = result.OrderBy(p => p.State.StateName);
+                }
+                else
+                    result = result.OrderByDescending(p => p.State.StateName);
+            }
+            if (field == "Address")
+            {
+                if (ascending)
+                {
+                    result = result.OrderBy(p => p.Address);
+                }
+                else
+                    result = result.OrderByDescending(p => p.Address);
+            }
+            return View("Index", result);
+       
+        }
+
+    }
+}
---
+++ PhoneDirectory/Controllers/ContactsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PhoneDirectory.Models;
+
+namespace PhoneDirectory.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            var contacts = db.Contacts.Include(c => c.AspNetUser).Include(c => c.Country).Include(c => c.State);
+            return View(contacts.ToList());
+        }
+
+        // GET: Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "CountryName");
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName");
+            return View();
+        }
+
+        // POST: Contacts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ContactId,ContactName,ContactNo1,ContactNo2,CountryId,StateId,Address,Image,UserId")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", contact.UserId);
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "CountryName", contact.CountryId);
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName", contact.StateId);
+            return View(contact);
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", contact.UserId);
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "CountryName", contact.CountryId);
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName", contact.StateId);
+            return View(contact);
+        }
+
+        // POST: Contacts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ContactId,ContactName,ContactNo1,ContactNo2,CountryId,StateId,Address,Image,UserId")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", contact.UserId);
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "CountryName", contact.CountryId);
+            ViewBag.StateId = new SelectList(db.States, "StateId", "StateName", contact.StateId);
+            return View(contact);
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
